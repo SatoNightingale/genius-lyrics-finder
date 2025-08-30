e173167e2b14c4c5f46f3834d12b9095e83bb160
SatoNightingale <davidmederosprieto@gmail.com>
2025-08-30 19:52:09 -0400

    Búsquedas en el server con httpx

--- backend/server.py
+++ backend/server.py
@@ -1,22 +1,25 @@
+import asyncio
 import os
 import json
 import sys
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+import httpx
 from pydantic import BaseModel
 import lyricsgenius
+import requests
 
 
 app = FastAPI()
 
-GENIUS_ACCESS_TOKEN = os.getenv("GENIUS_TOKEN")
+GENIUS_ACCESS_TOKEN = os.getenv("GENIUS_TOKEN", "SPqGfxsIk4OkUD2mKptJfGWxz-2bhjlcAIT0zAWfVACV5df3Hu5uz4ndVBfA7tws")
 
 if GENIUS_ACCESS_TOKEN:
     genius = lyricsgenius.Genius(
         GENIUS_ACCESS_TOKEN,
         remove_section_headers=True,
         skip_non_songs=False,
-        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0"
     )
 else:
     print("Error fatal: no se pudo iniciar genius")
@@ -30,13 +33,50 @@ class Cancion(BaseModel):
 class Payload(BaseModel):
     items: list[Cancion]
 
+def search_song_sync(titulo, artista, song_id):
+    return genius.search_song(title=titulo, artist=artista, song_id=song_id)
+
+async def search_song_async(titulo, artista, song_id):
+    song = await asyncio.to_thread(search_song_sync, titulo, artista, song_id)
+    return song
+
+async def request_song(titulo: str, artista: str):
+    base_url = "http://api.genius.com"
+    headers = {'Authorization': 'Bearer ' + GENIUS_ACCESS_TOKEN}
+    search_url = base_url + "/search"
+    song_title = titulo
+    params = {'q': song_title}
+    # response = requests.get(search_url, params=params, headers=headers)
+    async with httpx.AsyncClient() as cliente:
+        response = await cliente.get(search_url, params=params, headers=headers, follow_redirects=True)
+        data = response.json()
+    
+    # json = response.json()
+
+    # print(json)
+
+    # send the full title of the song
+    full_title = data['response']['hits'][0]['result']['full_title']
+    artist = data['response']['hits'][0]['result']['primary_artist']['name']
+    FullSearchTerm = f"{artist} {song_title}"
+    print(full_title)
+    print(FullSearchTerm)
+
+    # get the song ID
+    song_id = data['response']['hits'][0]['result']['id']
+    print(f"song_id is {song_id}")
+
+    song = await search_song_async(titulo=full_title, artista=artist, song_id=song_id)
+    print("lyrics:", song.lyrics)
+    return song
 
 async def buscar_cancion(cancion: Cancion):
     letra = ''
     error = ''
 
     try:
-        song_genius = genius.search_song(cancion.titulo, cancion.artista)
+        # song_genius = genius.search_song(cancion.titulo, cancion.artista)
+        song_genius = await request_song(cancion.titulo, cancion.artista)
 
         if song_genius and song_genius.lyrics:
             letra = song_genius.lyrics
@@ -47,7 +87,7 @@ async def buscar_cancion(cancion: Cancion):
     except Exception as e:
         error = "error_del_servidor"
         print(e)
-    
+    print("returning")
     return {"id": cancion.id, "letra": letra, "error": error}
 
 async def procesar_lista_canciones(canciones: list[Cancion]):
